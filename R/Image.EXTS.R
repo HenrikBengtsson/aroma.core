123b1383f2d3c27f64a36f103392a47e71b7f50e
Henrik Bengtsson <hb@aroma-project.org>
2011-01-31 15:40:45 +0000

    Moved all methods related to the Image class to one source file.  This way code relying on EBImage is in one file.  This will help simplifying the generalizing of the aroma code for generating bitmap images/PNG files.  We plan to support the 'png' package, either in parallel to, or as a replacement for, the EBImage package.
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/aroma-dots/pkg/aroma.core@2117 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ R/Image.EXTS.R
@@ -0,0 +1,448 @@
+##############################################################################
+# This source file contains all methods and classes related to the
+# Image class (of EBImage).
+##############################################################################
+
+
+###########################################################################/**
+# @set "class=matrix"
+# @RdocMethod as.GrayscaleImage
+# @aliasmethod as.TrueColorsImage
+#
+# @title "Creates a Grayscale (TrueColor) Image from a matrix file"
+#
+# \description{
+#  @get "title".
+# }
+#
+# @synopsis
+#
+# \arguments{
+#   \item{z}{A KxN @matrix.}
+#   \item{transforms}{A @list of transform @functions.}
+#   \item{interleaved}{A @character string specifying how the image data
+#     should be interleaved, if at all.}
+#   \item{scale}{A @numeric scale factor in (0,+Inf) for resizing the 
+#     imaging. If \code{1}, no resizing is done.}
+#   \item{...}{Passed to \code{colorize()} of @see "EBImage::Image".}
+#   \item{verbose}{A @logical or a @see "R.utils::Verbose" object.}
+# }
+#
+# \value{
+#   Returns an @see "EBImage::Image" object.
+# }
+#
+# \author{Henrik Bengtsson and Ken Simpson.}
+#
+# \seealso{
+#   @see "EBImage::Image".
+#   @seeclass
+# }
+#
+# @keyword IO
+#*/###########################################################################
+setMethodS3("as.GrayscaleImage", "matrix", function(z, transforms=NULL, interleaved=c("none", "h", "v", "auto"), scale=1, ..., verbose=FALSE) {
+  require("EBImage") || throw("Package not loaded: EBImage.");
+
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+  # Local functions
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+  safeMeans <- function(x) {
+    mean(x[is.finite(x)]);
+  }
+
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+  # Validate arguments
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+  # Argument 'transforms':
+  if (!is.null(transforms)) {
+    if (!is.list(transforms)) {
+      transforms <- list(transforms);
+    }
+  }
+
+  for (transform in transforms) {
+    if (!is.function(transform)) {
+      throw("Argument 'transforms' contains a non-function: ", 
+                                                        mode(transform));
+    }
+  }
+
+  # Argument 'interleaved':
+  interleaved <- match.arg(interleaved);
+
+  # Argument 'scale':
+  scale <- Arguments$getNumeric(scale, range=c(0,Inf));
+
+  # Argument 'verbose':
+  verbose <- Arguments$getVerbose(verbose);
+  if (verbose) {
+    pushState(verbose);
+    on.exit(popState(verbose));
+  }
+
+
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+  # Read data
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+  verbose && enter(verbose, "Creating Image object from matrix");
+
+  # Transform signals?
+  for (transform in transforms) {
+    dim <- dim(z);
+    z <- transform(z);
+    dim(z) <- dim;
+  }
+
+  # Transform into [0,1]
+  # EBImage: "Grayscale values are assumed to be in the range [0,1], 
+  # although this is not a requirement in sense of data storage. Although,
+  # many image processing functions will assume data in this range or will
+  # generate invalid results for the data out of this range."
+  verbose && enter(verbose, "Rescaling to [0,1]");
+  r <- range(z, na.rm=TRUE, finite=TRUE);
+  z <- (z - r[1])/(r[2]-r[1]);
+  verbose && summary(verbose, as.vector(z));
+  verbose && exit(verbose);
+
+  # Create an EBImage Image object
+  z <- t(z);
+  img <- EBImage::Image(data=z, dim=dim(z), colormode=EBImage::Grayscale);
+
+  # if only PM locations have signal, add a fake row?
+  img <- interleave(img, what=interleaved);
+
+  # Scale (if scale ==1, it does nothing)
+  img <- rescale(img, scale=scale);
+
+  verbose && exit(verbose);
+
+  img;
+}, protected=TRUE)
+
+
+setMethodS3("as.TrueColorImage", "matrix", function(z, ...) {
+  img <- as.GrayscaleImage(z, ...);
+  img <- colorize(img, ...);
+  img;
+}, protected=TRUE)
+
+
+setMethodS3("as.TrueColorImage", "Image", function(img, ...) {
+  colorMode <- colorMode(img);
+  if (colorMode == EBImage::TrueColor)
+    return(img);
+
+  img <- colorize(img, ...);
+
+  img;
+}, protected=TRUE)
+
+
+# given an input Image, transform to new RGB image based
+# on list of colours given in argument 'palette' and lower and
+# upper bounds given in 'lim'.
+setMethodS3("colorize", "Image", function(this, palette=gray.colors(256), lim=c(-Inf,Inf), outlierCol="white", ..., verbose=FALSE) {
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+  # Local functions
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+  col2rgbComposite <- function(color) {
+    rgb <- col2rgb(color);
+    rgb <- rgb[3,]*256^2 + rgb[2,]*256 + rgb[1,];
+    rgb;
+  } # col2rgbComposite()
+  
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+  # Validate arguments
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+  # Argument 'this':
+  colorMode <- colorMode(this);
+  if (colorMode != EBImage::Grayscale) {
+    if (colorMode == EBImage::TrueColor) {
+      colorMode <- "TrueColor";
+    }
+    throw("Cannot colorize() non-Grayscale Image: ", colorMode);
+  }
+
+  # Argument 'lim':
+
+  # Argument 'verbose':
+  verbose <- Arguments$getVerbose(verbose);
+  if (verbose) {
+    pushState(verbose);
+    on.exit(popState(verbose));
+  }
+
+
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+  # Bin the signals
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+  verbose && enter(verbose, "Binning image signals");
+  # Data
+  x <- this@.Data;
+  dim <- dim(x);
+
+  # Outliers
+  if (lim[1] > -Inf)
+    x[x < lim[1]] <- NA;
+  if (lim[2] < +Inf)
+    x[x > lim[2]] <- NA;
+  x[!is.finite(x)] <- NA;
+
+  # Standardize to [0,1]
+  r <- range(x, na.rm=TRUE);
+  verbose && cat(verbose, "Before:");
+  verbose && print(verbose, summary(as.vector(x)));
+  x <- (x - r[1])/(r[2]-r[1]);
+  verbose && cat(verbose, "After:");
+  verbose && print(verbose, summary(as.vector(x)));
+
+  # Standardize to [0,n] where n is number of bins
+  n <- length(palette);
+  x <- x*n;
+
+  # Bin to [1,n] by moving any values at the left extreme to bin one.
+  x <- as.integer(x);
+  x[x == 0] <- as.integer(1);
+  verbose && exit(verbose);
+
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+  # Assign colors to each bin
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+  verbose && enter(verbose, "Binning image data to color map");
+  binValue <- col2rgbComposite(palette);
+  verbose && cat(verbose, "Color map:");
+  verbose && str(verbose, binValue);
+  x <- binValue[x];
+  x[is.na(x)] <- col2rgbComposite(outlierCol);
+  verbose && str(verbose, x);
+
+
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+  # Create a new Image object
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+  img <- EBImage::Image(x, dim=dim, colormode=EBImage::TrueColor);
+
+  verbose && exit(verbose);
+
+  img;
+}, protected=TRUE)
+
+
+setMethodS3("rgbTransform", "Image", function(this, ...) {
+  colorize(this, ...);
+}, private=TRUE, deprecated=TRUE)
+
+
+setMethodS3("display", "Image", function(this, ...) {
+  EBImage::display(this, ...);
+}, protected=TRUE) 
+
+
+
+setMethodS3("interleave", "Image", function(this, what=c("none", "h", "v", "auto"), ..., verbose=TRUE) {
+  require("EBImage") || throw("Package not loaded: EBImage.");
+
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+  # Local functions
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+  safeMeans <- function(x) {
+    mean(x[is.finite(x)]);
+  }
+
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+  # Validate arguments
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+  # Argument 'what':
+  what <- match.arg(what);
+
+  # Argument 'verbose':
+  verbose <- Arguments$getVerbose(verbose);
+  if (verbose) {
+    pushState(verbose);
+    on.exit(popState(verbose));
+  }
+
+
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+  # Interleave
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+  # Nothing todo?
+  if (what == "none")
+    return(this);
+
+  verbose && enter(verbose, "Interleaving image");
+
+  # Get image data
+  z <- this@.Data;
+  verbose && cat(verbose, "z:");
+  verbose && str(verbose, z);
+  zDim <- dim(z);
+  ndim <- length(zDim);
+  dim(z) <- zDim;
+
+  # Sanity check
+  stopifnot(ndim == 2 || ndim == 3);
+  if (ndim == 2) {
+    dim(z) <- c(zDim, 1);
+  }
+
+  # if only PM locations have signal, add a fake row
+  if (what == "auto") {
+    verbose && enter(verbose, "Infering horizontal, vertical, or no interleaving");
+    n <- 2*(nrow(z) %/% 2);
+    idxOdd <- seq(from=1, to=n, by=2);
+    zOdd <- z[idxOdd,,,drop=FALSE];
+    zEven <- z[idxOdd+1,,,drop=FALSE];
+    hOdd <- safeMeans(abs(zOdd));
+    hEven <- safeMeans(abs(zEven));
+    verbose && printf(verbose, "hOdd=%.2g\n", hOdd);
+    verbose && printf(verbose, "hEven=%.2g\n", hEven);
+    hRatio <- log(hOdd/hEven);
+    verbose && printf(verbose, "hRatio=%.2g\n", hRatio);
+
+    n <- 2*(ncol(z) %/% 2);
+#    n <- max(n, 40);  # Infer from the first 40 rows.
+    idxOdd <- seq(from=1, to=n, by=2);
+    zOdd <- z[,idxOdd,,drop=FALSE];
+    zEven <- z[,idxOdd+1,,drop=FALSE];
+    vOdd <- safeMeans(abs(zOdd));
+    vEven <- safeMeans(abs(zEven));
+    verbose && printf(verbose, "vOdd=%.2g\n", vOdd);
+    verbose && printf(verbose, "vEven=%.2g\n", vEven);
+    vRatio <- log(vOdd/vEven);
+    verbose && printf(verbose, "vRatio=%.2g\n", vRatio);
+
+    what <- "none";
+    if (abs(vRatio) > abs(hRatio)) {
+      if (abs(vRatio) > 0.25) {
+        if (vRatio > 0)
+          what <- "v"
+        else
+          what <- "v";
+      }
+    } else {
+      if (abs(hRatio) > 0.25) {
+        if (hRatio > 0)
+          what <- "h"
+        else
+          what <- "h";
+      }
+    }
+    verbose && cat(verbose, "what: ", what);
+    verbose && exit(verbose);
+  }
+
+  isUpdated <- FALSE;
+  if (what == "h") {
+    idxOdd <- seq(from=1, to=2*(nrow(z) %/% 2), by=2);
+    z[idxOdd,,] <- z[idxOdd+1,,,drop=FALSE];
+  } else if (what == "v") {
+    idxOdd <- seq(from=1, to=2*(ncol(z) %/% 2), by=2);
+    z[,idxOdd,] <- z[,idxOdd+1,,drop=FALSE];
+  } else {
+    isUpdated <- FALSE;
+  }
+
+  if (ndim == 2) {
+    z <- z[,,1,drop=TRUE];
+  }
+
+  # Update?
+  if (isUpdated) {
+    this@.Data <- z;
+  }
+
+  verbose && exit(verbose);
+
+  this;
+})
+
+
+setMethodS3("rescale", "Image", function(this, scale=1, blur=FALSE, ..., verbose=FALSE) {
+  require("EBImage") || throw("Package not loaded: EBImage.");
+
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+  # Validate arguments
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+  # Argument 'scale':
+  scale <- Arguments$getNumeric(scale, range=c(0,Inf));
+
+  # Argument 'verbose':
+  verbose <- Arguments$getVerbose(verbose);
+  if (verbose) {
+    pushState(verbose);
+    on.exit(popState(verbose));
+  }
+
+
+  # Nothing to do?
+  if (scale == 1)
+    return(this);
+
+  verbose && enter(verbose, "Rescaling image");
+  verbose && sprintf(verbose, "Scale: %.2g\n", scale);
+  img <- EBImage::resize(this, w=scale*dim(this)[1], blur=blur);
+  verbose && exit(verbose);
+
+  img;
+})
+
+
+setMethodS3("writeImage", "Image", function(x, file, ...) {
+  if (compareVersion(packageDescription("EBImage")$Version, "2.1.23") >= 0) {
+    EBImage::writeImage(x, files=file, ...);
+  } else {
+    EBImage::write.image(x, files=file, ...);
+  }
+}, protected=TRUE)
+
+
+############################################################################
+# HISTORY:
+# 2011-01-31
+# o Moved all methods related to the Image class to one source file.
+#   All history has been merged accordingly.
+# 2011-01-30
+# o Added writeImage() in order to clean up aroma.affymetrix, cf. 
+#   writeImage() for AffymetrixCelFile. 
+# 2010-06-22
+# o BUG FIX: as.GrayscaleImage(..., transforms=NULL) for 'matrix' would
+#   throw "Exception: Argument 'transforms' contains a non-function: NULL".
+# 2009-05-16
+# o Now argument 'scale' of as.GrayscaleImage() is validated using 
+#   Arguments$getNumeric() [not getDouble()].
+# o Now rescale() for Image uses Arguments$getNumerics(), not 
+#   getDoubles(), where possible.  This will save memory in some cases.
+# 2009-05-10
+# o Forgot argument 'verbose' for interleave().
+# o BUG FIX: interleave() for Image gave 'Error in z[idxOdd,, ] : incorrect 
+#   number of dimensions'.  The internal image structure is now a 2-dim 
+#   matrix, again(?!?).
+# 2008-10-16
+# o BUG FIX: Tried to turn a function passed by 'transforms' to a list using
+#   as.list() and not list().
+# 2008-04-13
+# o Added asserting to colorize() that the Image has color mode Grayscale.
+# o Renamed rgbTransform() to colorize().
+# 2008-04-14
+# o Added display().
+# 2008-05-10
+# o BUG FIX: interleave() for Image gave 'Error in z[idxOdd, ] : incorrect 
+#   number of dimensions'.  The internal image structure is a 3-dim array.
+# 2008-03-14
+# o Added rescale() from getImage() of AffymetrixCelFile.
+# o Added interleave() from from getImage() of AffymetrixCelFile.
+# o Added getImage().
+# o Added as.GrayscaleImage() and as.TrueColorImage().
+# o Created from getImage() of AffymetrixCelFile.
+# 2007-06-11
+# o Explicit calls to EBImage::Image() etc in colorize().
+# 2007-01-30 /HB
+# o Improved the speed of colorize() even further by dirt simple binning.
+# o Move to Image.rgbTransform.R.
+# o Improved speed of colorize() on the internal binning in rgbTransform().
+# 2007-01-12 /KS
+# o TOFIX: THIS [colorize()] IS CURRENTLY TOO SLOW.
+# o Created. colorize() is used by getImage() in AffymetrixCelFile.
+############################################################################
