dcda35896f870189cdf8242858f7acf49229adfa
Henrik Bengtsson <hb@aroma-project.org>
2011-03-02 03:42:31 +0000

    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/aroma-dots/pkg/aroma.core@2164 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/AromaTabularBinaryFile.R
+++ R/AromaTabularBinaryFile.R
@@ -1047,6 +1047,12 @@ setMethodS3("allocate", "AromaTabularBinaryFile", function(static, filename, pat
     throw("Argument 'footer' must be NULL or a list: ", class(footer)[1]);
   }
 
+  # Argument 'overwrite':
+  overwrite <- Arguments$getLogical(overwrite);
+
+  # Argument 'skip':
+  skip <- Arguments$getLogical(skip);
+
   # Argument 'verbose':
   verbose <- Arguments$getVerbose(verbose);
   if (verbose) {
@@ -1086,6 +1092,13 @@ setMethodS3("allocate", "AromaTabularBinaryFile", function(static, filename, pat
   # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
   # Create empty temporary file
   # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  # Overwrite?
+  if (overwrite && isFile(pathname)) {
+    # TODO: Added a backup/restore feature in case new writing fails.
+    file.remove(pathname);
+    verbose && cat(verbose, "Removed pre-existing file (overwrite=TRUE).");
+  }
+
   pathnameT <- pushTemporaryFile(pathname, verbose=verbose);
 
   con <- file(pathnameT, open="wb");
