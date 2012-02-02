1fb390df63d6da8e946ff51f598eebde22887e83
Henrik Bengtsson <hb@aroma-project.org>
2012-02-02 19:57:34 +0000

    Version: 2.4.5 [2012-02-02] o GENERALIZATION: As ChromosomeExplorer v3.3, the new ArrayExplorer v3.3   works with most common web browsers. See below.  However, we still   have problems getting ArrayExplorer to load the main spatil (PNG) image   in Internet Explorer.
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/aroma-dots/pkg/aroma.core@2340 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- inst/reports/includes/js/Scrollbar2d.js
+++ inst/reports/includes/js/Scrollbar2d.js
@@ -1,154 +1,124 @@
 /****************************************************************
- * ScrollImage2d()
+ * Scrollbar2d()
  *
  * Author: Henrik Bengtson, hb@stat.berkeley.edu
  ****************************************************************/
-function ScrollImage2d(id) {
+function Scrollbar2d(id) {
   this.setImage = function(url) {
     var owner = this;
 
-    /* Image loader to get the size of the (non-rescaled) image */
-//    var myImage = new Image();
-//    this.myImage = myImage;
-//    myImage.onload = function() {
-//      owner.imageWidth = this.width;
-//      owner.imageHeight = this.height;
-////alert(owner.imageWidth + " x " + owner.imageHeight);
-//    }
-//    myImage.src = url;
-
+    /* Define onload() function */
     this.image.onload = function() {
       owner.onLoad();
       owner.update();
-      owner.imageWidth = this.width;
-      owner.imageHeight = this.height;
     }
 
     /* Define onload() function */
     this.image.onerror = function() {
-      /* alert('Image not loaded: ' + url); */
+    /* alert('Image not loaded: ' + url); */
     }
 
     /* Start loading image */
     this.image.src = url;
   }
 
-  this.getImageDimension = function() {
-//alert(this.myImage.width + " x " + this.myImage.height);
-    var res = Object();
-//      alert(this.image.clientWidth);
-    res.width = this.imageWidth;
-    res.height = this.imageHeight;
-    return(res);
-  }
-
   this.getImageWidth = function() {
-    return(this.image.width);
+    return(this.image.width-5);
   }
 
   this.getImageHeight = function() {
-    return(this.image.height);
-  }
-
-  this.getAspectRatio = function() {
-    return (this.container.clientWidth / this.container.clientHeight);
+    return(this.image.height-4);
   }
 
   this.update = function() {
-    this.container.scrollLeft = this.x * this.container.scrollWidth;
-    this.container.scrollTop = this.y * this.container.scrollHeight;
     var pos = findXY(this.image);
     this.xOffset = pos.x;
     this.yOffset = pos.y;
     var w = this.getImageWidth();
     var h = this.getImageHeight();
-    this.image.style.left = Math.round(this.xOffset + w*this.x) + "px";
-    this.image.style.top = Math.round(this.yOffset + h*this.y) + "px";
-    this.image.width = this.width * this.container.clientWidth;
-    var dim = this.getImageDimension();
-    var aspect = this.width / this.height;
-    this.image.height = aspect * this.height * this.container.clientWidth;
-    /*
-    this.container.style.height = winAspect*this.container.clientWidth;
-    */
-  }
-
-  this.getXY = function() {
-    var w = this.getImageWidth()/this.width;
-    var h = this.getImageHeight()/this.height;
-    var res = Object();
-    res.x = Math.round(w*this.x);
-    res.y = Math.round(w*this.y);
-    return(res);
-  }
-
-  this.getDimension = function() {
-    var res = Object();
-    var dim = this.getImageDimension();
-    res.width = Math.round(dim.width);
-    res.height = Math.round(dim.height);
-    return(res);
+    this.marker.style.width = Math.round(w*this.width) + "px";
+    this.marker.style.height = Math.round(h*this.height) + "px";
+    this.marker.style.left = Math.round(this.xOffset + w*this.x) + "px";
+    this.marker.style.top = Math.round(this.yOffset + h*this.y) + "px";
+    this.marker.style.border = 'solid; black; 2px';
   }
 
   this.getRegion = function() {
-    var xy = this.getXY();
-    var dim = this.getDimension();
     var res = Object();
-    res.x0 = xy.x;
-    res.y0 = xy.y;
-    res.x1 = xy.x + dim.width;
-    res.y1 = xy.y + dim.height;
-    return(res);
+    res.x0 = this.x;
+    res.y0 = this.y;
+    res.x1 = this.x + this.width;
+    res.y1 = this.y + this.height;
+    return res;
   }
 
   this.setRelXY = function(x,y) {
     x = Math.max(0, x);
-    x = Math.min(x, 1);
+    x = Math.min(x, 1-this.width);
     y = Math.max(0, y);
-    y = Math.min(y, 1);
+    y = Math.min(y, 1-this.height);
     this.x = x;
     this.y = y;
   }
 
   this.setSize = function(scale) {
     this.setRelDimension(scale, scale);
-    this.update();
   }
 
   this.setRelDimension = function(width, height) {
+    var xMid = this.x + this.width/2;
+    var yMid = this.y + this.height/2;
     width = Math.max(0, width);
     height = Math.max(0, height);
+    width = Math.min(width, 1);
+    height = Math.min(height, 1);
     this.width = width;
     this.height = height;
+    this.setRelXY(xMid - this.width/2, yMid - this.height/2);
   }
 
   this.setCursor = function(status) {
+    this.marker.style.cursor = status;
+  /*
     this.container.style.cursor = status;
     this.image.style.cursor = status;
+  */
   }
 
   this.setupEventHandlers = function() {
     var owner = this;
 
-    this.image.onmousedown = function() {
+    var containerOnClick = function() {
+      var w = owner.getImageWidth();
+      var h = owner.getImageHeight();
       var e = arguments[0] || event;
+      var pos = findXY(owner.marker);
+      var dx = (e.clientX - pos.x)/w - owner.width;
+      var dy = (e.clientY - pos.y)/h - owner.height;
+      owner.setRelXY(dx,dy);
+      owner.update();
+    }
+
+    /*
+    this.container.onclick = containerOnClick;
+    */
+
+    this.marker.onmousedown = function() {
       var x0 = owner.x;
       var y0 = owner.y;
       var w = owner.getImageWidth();
       var h = owner.getImageHeight();
+      var e = arguments[0] || event;
       var xStart = e.clientX;
       var yStart = e.clientY;
-      var x = owner.container.scrollLeft + e.clientX;
-      var y = owner.container.scrollTop + e.clientY;
-      owner.setCursor("move");
-      owner.image.onmousemove = null;
+      owner.setCursor('move');
       owner.onmousedown();
   
       document.onmousemove = function() {
         var e = arguments[0] || event;
         var dx = (e.clientX - xStart)/w;
         var dy = (e.clientY - yStart)/h;
-        owner.setRelXY(x0-dx, y0-dy);
+        owner.setRelXY(x0+dx, y0+dy);
         owner.update();
         owner.onmousemove();
         return false;
@@ -156,8 +126,7 @@ function ScrollImage2d(id) {
   
       document.onmouseup = function() {
         document.onmousemove = null;
-        owner.image.onmousemove = null;
-        owner.setCursor("default");
+        owner.setCursor('default');
         owner.onmouseup();
         return false;
       }
@@ -171,8 +140,8 @@ function ScrollImage2d(id) {
   this.onmousemove = function() {}
   this.onmouseup = function() {}
 
+
   /* Initialize */
-  this.scale = 1;
   this.x = 0;
   this.y = 0;
   this.xOffset = 0;
@@ -182,19 +151,18 @@ function ScrollImage2d(id) {
   
   this.container = document.getElementById(id);
   this.image = document.getElementById(id + 'Image');
+  this.marker = document.getElementById(id + 'Marker');
   this.imageWidth = 0;
   this.imageHeight = 0;
 
   this.setupEventHandlers();
-} /* ScrollImage2d() */
+} /* Scrollbar2d() */
 
 
 /****************************************************************
  HISTORY:
  2012-02-02
- o KNOWN ISSUES: Internet Explorer fails to detect image size,
-   i.e. it is always zero, even in onload() of the image.
- o Extracted ScrollImage2d.js.
+ o Extracted Scrollbar2d.js.
  2008-06-23
  o Removed alert() for this.image.onerror(), because in 
    Firefox 3 these showed up although the image was loaded.
