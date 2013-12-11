d30cacddbe598b94761b388d63c0d09a9be293a6
Henrik Bengtsson <hb@aroma-project.org>
2013-12-11 22:19:01 +0000

    Version: 2.11.2 [2013-12-11] o DOCUMENTATION: Added documentation for exportAromaUnitPscnBinarySet().
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/aroma-dots/pkg/aroma.core@3416 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/AromaUnitTotalCnBinarySet.exportAromaUnitPscnBinarySet.R
+++ R/AromaUnitTotalCnBinarySet.exportAromaUnitPscnBinarySet.R
@@ -1,3 +1,42 @@
+###########################################################################/**
+# @RdocGeneric exportAromaUnitPscnBinarySet
+# @alias exportAromaUnitPscnBinarySet.AromaUnitTotalCnBinarySet
+# @alias exportAromaUnitPscnBinarySet.list
+#
+# @title "Export total and allele B signal data sets as a unified parent-specific copy number signal data set"
+#
+# \description{
+#  @get "title", where each sample is stored in one data file (contrary to the input data sets where each sample is stored in two separated files).
+# }
+#
+# @synopsis
+#
+# \arguments{
+#   \item{dsT, dsB}{An @see AromaUnitTotalCnBinarySet and an @see AromaUnitFracBCnBinarySet with the coupled sets of samples that match up by name.  If they don't match up, an exception is thrown.  The allele B fraction (BAF) data set \code{dsB} is by default inferred from the total CN data set \code{dsT}.}
+#   \item{dataSet, tags}{The name and tags of the output data set.
+#     The default is to infer those from the input \code{dsT} data set.}
+#   \item{...}{Not used.}
+#   \item{rootPath}{The root path of the output data set.}
+#   \item{overwrite, skip}{Specifies whether to overwrite and/or skip already exported samples.}
+#   \item{verbose}{See @see "R.utils::Verbose".}
+# }
+#
+# \value{
+#  Returns an @see AromaUnitPscnBinarySet object.
+# }
+#
+# \section{Allele-specific CRMAv2 pipeline}{
+#   A common use case is to run allele-specific CRMAv2, e.g.
+#   \code{dsNList <- doASCRMAv2(csR)}, which outputs a @list \code{dsNList}
+#   with elements corresponding to \code{dsT} and \code{dsB}.  This output
+#   can be exported to @see AromaUnitPscnBinarySet by this method as
+#   \code{dsN <- exportAromaUnitPscnBinarySet(dsNList)}.
+# }
+#
+# @author
+#
+# @keyword internal
+#*/###########################################################################
 setMethodS3("exportAromaUnitPscnBinarySet", "AromaUnitTotalCnBinarySet", function(dsT, dsB="*", dataSet="*", tags="*", ..., rootPath="totalAndFracBData/", overwrite=!skip, skip=TRUE, verbose=FALSE) {
   # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
   # Validate arguments
@@ -185,6 +224,8 @@ setMethodS3("exportAromaUnitPscnBinarySet", "list", function(dsList, ...) {
 
 ############################################################################
 # HISTORY:
+# 2013-12-11
+# o DOCUMENTATION: Added documentation for exportAromaUnitPscnBinarySet().
 # 2013-08-12
 # o BUG FIX: exportAromaUnitPscnBinarySet() for AromaUnitTotalCnBinarySet
 #   would throw an error on "unknown argument 'names' to indexOf()".
